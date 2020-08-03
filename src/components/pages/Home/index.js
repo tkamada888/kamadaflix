f5c7b8fba26a5146be604d5b87234362e9724707
Thiago <thiago.kamada@gmail.com>
2020-08-03 15:49:33 +0900

    Commit com novas telas

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer'
+import Menu from '../../Menu'
+import dadosIniciais from '../../../data/dados_iniciais.json'
+import BannerMain from '../../BannerMain'
+import Carousel from '../../Carousel'
+import Footer from '../../Footer'
 
-function App() {
+function Home() {
   return (
-    <div style={{background:"#141414"}}>
+    <div style={{ background: "#141414" }}>
       <Menu />
 
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que e front-end"}
+        videoDescription={"Testando o carousel"}
       />
 
       <Carousel
@@ -40,9 +40,9 @@ function App() {
       <Carousel
         category={dadosIniciais.categorias[5]}
       />
-
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import CadastroVideo from './components/pages/cadastro/Video';
+import CadastroCategoria from './components/pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={Home} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route component={() =>(<div>Pagina 404</div>) } />  {/* Erro 404 */}
+  </Switch>
+  </BrowserRouter>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
